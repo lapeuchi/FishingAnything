e5e77a6c52ec0641b80f19eb7be03298b5c3fec9
RiceSnack <95210265+RiceSnack@users.noreply.github.com>
2021-11-29 19:30:46 +0900

    .
    .

---
+++ FhsingAnything/Assets/Scripts/MapManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MapManager : MonoBehaviour
+{
+    // 선언
+    public GameObject Player;
+    void Start()
+    {
+        Player.transform.position = new Vector2(2.84f , -2.83f);
+    }
+}
