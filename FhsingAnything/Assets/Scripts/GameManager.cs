d24075f0906dead7c85ac39e37da805f8c373ded
lapeuchi <78643629+lapeuchi@users.noreply.github.com>
2021-11-30 18:09:27 +0900

    Revert "j"
    This reverts commit 345c4da5bd610090bfc2f121da6d15b7d1af2f68.

--- FhsingAnything/Assets/Scripts/GameManager.cs
+++ FhsingAnything/Assets/Scripts/GameManager.cs
@@ -8,7 +8,6 @@ public class GameManager : MonoBehaviour
 
     public enum GameState { Lobby, Fishing, shop, Setting};
     public GameState curGameState = GameState.Lobby;
-    public List<GameObject> fishL;
     void Awake()
     {
         if (instance == null)
