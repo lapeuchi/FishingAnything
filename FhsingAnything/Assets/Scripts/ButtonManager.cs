da55679416b9c50eac43a685d1b3d821d9e3fd64
RiceSnack <95210265+RiceSnack@users.noreply.github.com>
2021-11-30 19:42:37 +0900

    2021_11_30
    디자인

---
+++ FhsingAnything/Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour
+{
+    public GameObject SelectPlaceBackGround;
+
+    public void GoSea() {
+        SceneManager.LoadScene("SeaScene");
+    }
+    public void GoRiver()
+    {
+        SceneManager.LoadScene("RiverScene");
+    }
+    public void GoArctic()
+    {
+        SceneManager.LoadScene("ArcticScene");
+    }
+    public void GoBaclocdam()
+    {
+        SceneManager.LoadScene("BaclocdamScene");
+    }
+    public void GoBaths()
+    {
+        SceneManager.LoadScene("BathsScene");
+    }
+    public void GoSewer()
+    {
+        SceneManager.LoadScene("SewerScene");
+    }
+    public void GoWaterFall()
+    {
+        SceneManager.LoadScene("WaterFallScene");
+    }
+    public void GoFishMarket()
+    {
+        SceneManager.LoadScene("FishMarketScene");
+    }
+    public void GoShop()
+    {
+        SceneManager.LoadScene("ShopScene");
+    }
+    public void SelectPlaceOn()
+    {
+        SelectPlaceBackGround.SetActive(true);
+    }
+    public void SelectPlaceFalse()
+    {
+        SelectPlaceBackGround.SetActive(false);
+    }
+}
--- FhsingAnything/Assets/Scripts/MapManager.cs
+++ FhsingAnything/Assets/Scripts/MapManager.cs
@@ -5,10 +5,5 @@ using UnityEngine.SceneManagement;
 
 public class MapManager : MonoBehaviour
 {
-    // 선언
-    public GameObject Player;
-    void Start()
-    {
-        Player.transform.position = new Vector2(2.84f , -2.83f);
-    }
+    
 }
