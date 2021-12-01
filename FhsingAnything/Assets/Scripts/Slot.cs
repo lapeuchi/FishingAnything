69f203a07af8b953775c9ea0f133a47627f41b7f
RiceSnack <95210265+RiceSnack@users.noreply.github.com>
2021-12-01 20:46:45 +0900

    2021_12_01_20:46
    인벤토리 작업중

---
+++ FhsingAnything/Assets/Scripts/Slot.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Slot : MonoBehaviour
+{
+    [SerializeField] Image Image;
+
+    private Item _item;
+    public Item item
+    {
+        get
+        { return _item; }
+        set
+        {
+            _item = value;
+            if(_item != null)
+            {
+                Image.sprite = item.itemImage;
+                Image.color = new Color(1, 1, 1, 1);
+            }
+            else
+            {
+                Image.color = new Color(1, 1, 1, 0);
+            }
+        }
+    }
+}
