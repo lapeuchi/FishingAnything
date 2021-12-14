7bdfcd6981e4c023e86218f707f747119650d426
lapeuchi <78643629+lapeuchi@users.noreply.github.com>
2021-12-14 16:35:49 +0900

    g
    g

--- FhsingAnything/Assets/Scripts/Fishing/Fish.cs
+++ FhsingAnything/Assets/Scripts/Fishing/Fish.cs
@@ -8,14 +8,23 @@ public class Fish : MonoBehaviour
     public int money;
     public int size;
 
-    void Update()
-    {
-    
-    }
-
     public void Start()
     {
-        FishManager.instance.FishTier = (FishManager.Tier)Random.Range(0, 6);
         FishManager.instance.SetStatus(FishManager.instance.FishTier, ref hp, ref money, ref size);
+        StartCoroutine(Set());
+    }
+
+    void Update()
+    {
+        if (FishManager.instance.isSucess == true) 
+        {
+            gameObject.SetActive(true);   
+        }
+    }
+
+    IEnumerator Set()
+    {     
+        yield return new WaitForSeconds(0.01f);
+        gameObject.SetActive(false);
     }
 }
