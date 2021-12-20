00ad890fa2ea508a0511a12ff33d1b752f53c79f
lapeuchi <78643629+lapeuchi@users.noreply.github.com>
2021-12-20 19:44:07 +0900

    f
    f

--- FhsingAnything/Assets/1_Scripts/Money.cs
+++ FhsingAnything/Assets/1_Scripts/Money.cs
@@ -13,6 +13,6 @@ public class Money : MonoBehaviour
     }
     public void moneyUpdate()
     {
-        moneyUI.text = GameManager.instance.money.ToString();
+        moneyUI.text = GameManager.instance.Money.ToString();
     }
 }
