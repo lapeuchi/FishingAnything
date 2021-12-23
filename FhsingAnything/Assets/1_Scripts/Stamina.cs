9c6d1e6fc7e1b513b8f08105c264cca724cd9d5b
lapeuchi <78643629+lapeuchi@users.noreply.github.com>
2021-12-23 13:35:33 +0900

    s
    s

--- FhsingAnything/Assets/1_Scripts/Stamina.cs
+++ FhsingAnything/Assets/1_Scripts/Stamina.cs
@@ -1,18 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class Stemina : MonoBehaviour
+public class Stamina : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        gameObject.GetComponent<Text>().text = "피로도:" + GameManager.instance.Stamina + "/" + GameManager.instance.Max_Stamina;               
     }
 }
